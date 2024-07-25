5430d5bdb5a9071542647122994c846549a7b698
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-07-25 09:17:20 +0200

    Final Upload

--- PlayCode/gettingStarted/Main.java
+++ PlayCode/gettingStarted/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
         //first way to create a thread -> make a subclass of the Thread class
         //Use this if you don't need to use inheritance since a class can only have one parent class
@@ -10,7 +10,12 @@ public class Main {
         MyRunnable runnable1 = new MyRunnable();
         Thread thread2 = new Thread(runnable1);
 
+        thread1.setDaemon(true); // this sets it to a daemon thread so it is no longer a user thread
+        //JVM does not wait for daemon threads to complete before exiting the program.
+        //as long as there is a user thread JVM will wait until last user thread dies.
+
         thread1.start();
+        //thread1.join(); // calling thread (main) will wait until the given thread dies or until specified miliseconds is done
         thread2.start();
     }
 }
