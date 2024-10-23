f3811afc49e722bab79cd98f5bf304d16aeb6d17
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-23 08:43:47 +0200

    add LockFreeStack implementation

--- PlayCode/chapter11/LockFreeStack.java
+++ PlayCode/chapter11/LockFreeStack.java
@@ -1,1 +1,49 @@
+include java.util.*;
 
+public class LockFreeStack<T extends Compareable<T>>{
+    AtomicReference<Node<T>> top = new AtomicReference<Node<T>>(null);
+    static final int MIN_DELAY = 10;
+    static final int MAX_DELAY = 1000;
+    Backoff backoff = new Backoff(MIN_DELAY,MAX_DELAY);
+
+    protected boolean tryPush(Node<T> node){
+        Node<T> old = top.get();
+        node.next = old;
+        return(top.compareAndSet(old,node));
+    }
+
+    public void push(T value) {
+        Node<T> node = new Node<T>(value);
+        while(true){
+            if(tryPush(node)){
+                return;
+            } else {
+                backoff.backoff();
+            }
+        }
+    }
+
+    protected Node<T> tryPop() throws EmptyException {
+        Node<T> old = top.get();
+        if(old == null) throw new EmptyException();
+
+        Node<T> newTop = old.next;
+        if(top.comapreAndSet(old,newTop)){
+            return old;
+        } else {
+            return null;
+        }
+    }
+
+    public T pop() throws EmptyException {
+        while(true) {
+            try {
+                Node<T> returnNode = tryPop();
+                if(returnNode != null) return returnNode.value;
+                backoff.backoff();
+            } catch (Exception e) {
+                throw e;
+            }
+        }
+    }
+}
---
+++ PlayCode/chapter11/Node.java
@@ -0,0 +1,10 @@
+
+public class Node<T extends Comparable<T>> {
+    public T value;
+    public Node<T> next;
+
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+    }
+}
